1b123e92c512cf421027f8f0fce7e1f3da00f3ae
Alex <Abdulla>
2018-11-07 20:21:43 -0800

    Initial Commit

---
+++ MemoryBank2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MemoryBank2
+//
+//  Created by Alex Abdulla on 2018-11-07.
+//  Copyright © 2018 Alex Abdulla. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MemoryBank2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MemoryBank2
+//
+//  Created by Alex Abdulla on 2018-11-07.
+//  Copyright © 2018 Alex Abdulla. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
