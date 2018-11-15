ba1348ed6b7d906ec99eb4b031c64f84536d6985
Alex <Abdulla>
2018-11-14 20:52:16 -0800

    Added A Table View That Pulls From Firebase

--- MemoryBank2/FirstViewController.swift
+++ MemoryBank2/FirstViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 import FirebaseCore
 import Firebase
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+    
 
     @IBOutlet weak var userLabel: UITextField!
+    
+    @IBOutlet weak var tabelView: UITableView!
+    
+    var ref:DatabaseReference?
+    var postData = [String]()
     
     @IBAction func logOut(_ sender: Any) {
         do {
@@ -24,14 +33,46 @@ class FirstViewController: UIViewController {
         
     }
     
+    @IBOutlet var tableView: [UITableView]!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("user is", Auth.auth().currentUser?.email)
         userLabel.text = "USER" + (Auth.auth().currentUser?.email)!
         
+        
+        
+
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //set firebase ref
+        ref = Database.database().reference()
+        
+        //retreive posts and listen.
+        ref?.child("posts").observe(.childAdded, with: {
+                (snapshot) in
+            let post = snapshot.value as? String
+            
+            if let actualPost = post {
+                self.postData.append(actualPost)
+                self.tabelView.reloadData()
+            }
+            
+            print(self.postData)
+        })
+        
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = postData[indexPath.row]
+        
+        return(cell)
+        
+    }
 
 
 }
